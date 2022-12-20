98d43cbf209d8cff40a42ecc314ba5988e43cf29
anthonyqsun <qsun30@stuy.edu>
2022-12-20 18:36:15 -0500

    add api changes

--- app/utils/lol_api.py
+++ app/utils/lol_api.py
@@ -4,46 +4,43 @@ import requests
 class LOLApi:
     def __init__(self):
         self.champion_url = 'http://ddragon.leagueoflegends.com/cdn/12.23.1/data/en_US/champion.json'
-        self.champion_list = self.get_champion_list()
+        self.champion_list = self.__get_champion_list()
 
-    def get_champion_list(self):
+    def __get_champion_list(self):
         response = requests.get(self.champion_url)
         return response.json()['data']
 
-    def get_champion_data(self, champion_name):
+    def __get_champion_data(self, champion_name):
         return self.champion_list[champion_name]
 
     def attributes(self, champion_name):
-        tags = self.get_champion_data(champion_name)['tags']
+        tags = self.__get_champion_data(champion_name)['tags']
         return tags
 
     def partype(self, champion_name):
-        partype = self.get_champion_data(champion_name)['partype']
+        partype = self.__get_champion_data(champion_name)['partype']
         if partype == 'Mana':
             return 'Normal'
-        else:
-            return 'Weird'
+        return 'Weird'
 
     def body_type(self, champion_name):
         # Get armor and spell block
-        armor = self.get_champion_data(champion_name)['stats']['armor']
-        spell_block = self.get_champion_data(champion_name)['stats']['spellblock']
+        armor = self.__get_champion_data(champion_name)['stats']['armor']
+        spell_block = self.__get_champion_data(champion_name)['stats']['spellblock']
         if armor > 35 and spell_block > 31:
             return 'Big'
-        else:
-            return 'Skinny'
+        return 'Skinny'
 
     def strength(self, champion_name):
         # Get attack damage and attack damage per level
-        attack_damage = self.get_champion_data(champion_name)['stats']['attackdamage']
-        attack_damage_per_level = self.get_champion_data(champion_name)['stats']['attackdamageperlevel']
+        attack_damage = self.__get_champion_data(champion_name)['stats']['attackdamage']
+        attack_damage_per_level = self.__get_champion_data(champion_name)['stats']['attackdamageperlevel']
         if attack_damage > 55 and attack_damage_per_level > 3:
             return 'Strong'
-        else:
-            return 'Kind'
+        return 'Kind'
 
     def get_champion_image(self, champion_name):
-        champion = self.get_champion_data(champion_name)
+        champion = self.__get_champion_data(champion_name)
         image = "https://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/" + champion['image']['full']
         return image
 
